5acc4887bbc5b5d02303d38bc3b62986d01a0221
whitneywishart <whitneywishart@gmail.com>
2023-01-22 14:03:29 -0800

    begin js refactor to support single html page

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,83 @@
 //WHEN I click the start button
 //THEN a timer starts and I am presented with a question
 
-//Click start quiz button and start the timer
 //Start quiz and timer
-var timeElement = document.querySelector("#start-quiz");
-var timeDisplay = document.querySelector("#time-display");
-var secondsElapsed = 0;
+var timeDisplay = document.getElementById("time-display");
+var secondsLeft = 300;
 
-function startTimer() {
+function begin() {
+    sendQuiz();
     var timerInterval = setInterval(function () {
-        secondsElapsed++;
-        timeDisplay.textContent = "Time: " + secondsElapsed;
-
+        secondsLeft--;
+        timeDisplay.textContent = "Time: " + secondsLeft;
     }, 1000);
 }
 
-//Questions and options
-var page01Question = {
-    question01: "Commonly used data types DO NOT include:",
-    options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-    correct: 2
+
+//Display intro and start quiz button
+var quizIntro = {
+    title: "Coding Quiz Challenge",
+    description: "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!",
+    startButton: "Start Quiz"
+}
+
+function displayIntro(d) {
+    var introTitleDiv = document.getElementById("title");
+    var introDescriptionDiv = document.getElementById("description");
+
+    introTitleDiv.textContent = d.title;
+    introDescriptionDiv.textContent = d.description;
+
+    var startQuiz = document.getElementById("start-quiz");
+    startQuiz.textContent = "Start Quiz"
+}
+
+displayIntro(quizIntro);
+
+
+
+//Display first question on start quiz button click
+function sendQuiz() {
+    var firstQuestion = {
+        question01: "Commonly used data types DO NOT include:",
+        options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        correct: 2
+    }
+
+
+    function displayQuestion(q) {
+        var question01Div = document.getElementById("question01");
+        question01Div.textContent = q.question01;
+
+        var opts = document.querySelectorAll(".option-button");
+
+        opts.forEach(function (element, index) {
+            element.textContent = q.options[index];
+            element.addEventListener("click", function () {
+                if (q.correct == index) {
+                    console.log("Correct!");
+                } else {
+                    console.log("Wrong!");
+                }
+            })
+        });
+
+
+    }
+
+    displayQuestion(firstQuestion);
+
 }
 
 
-function displayQuestion(q) {
-    var question01Div = document.getElementById("question01");
-    question01Div.textContent = q.question01;
-
-    var opts = document.querySelectorAll(".option-button");
-    opts.forEach(function (element, index) {
-        element.textContent = q.options[index];
-        element.addEventListener("click", function () {
-            if (q.correct == index) {
-                console.log("Correct!");
-            } else {
-                console.log("Wrong!");
-            }
-        })
-    });
 
 
-}
 
-displayQuestion(page01Question);
+
+
+
+
+
 
 
 
